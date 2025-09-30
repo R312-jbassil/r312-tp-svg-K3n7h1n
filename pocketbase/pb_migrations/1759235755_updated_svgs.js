98218e4bef03e011a16c0dcbef998bfae6322c4e
Enzo Locatelli <enzort70290@gmail.com>
2025-09-30 15:38:34 +0200

    feat: integrate PocketBase for SVG management and enhance gallery functionality

---
+++ pocketbase/pb_migrations/1759235755_updated_svgs.js
@@ -0,0 +1,44 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3423567578")
+
+  // remove field
+  collection.fields.removeById("text1807006754")
+
+  // add field
+  collection.fields.addAt(3, new Field({
+    "hidden": false,
+    "id": "json1807006754",
+    "maxSize": 0,
+    "name": "chat_history",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "json"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3423567578")
+
+  // add field
+  collection.fields.addAt(3, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1807006754",
+    "max": 0,
+    "min": 0,
+    "name": "chat_history",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // remove field
+  collection.fields.removeById("json1807006754")
+
+  return app.save(collection)
+})
---
+++ src/utils/pocketbase-types.js
@@ -0,0 +1,18 @@
+
+
+export const Collections = {
+    Svg: "svgs",
+};
+
+export const SvgRecord = {
+    id: '',
+    name: '',
+    chat_history: '',
+    code: '',
+    creator: '',
+    updated: '',
+    collectionId: '',
+    collectionName: ''
+};
+
+export default Collections;
